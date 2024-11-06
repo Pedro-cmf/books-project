aba089ff07caafccba507363af22fd487205999f
Pedro César <pedroocesarmf@gmail.com>
2024-11-06 00:13:21 -0300

    add: componente de livros novos

--- src/componentes/principal/UltimosLancamentos/index.js
+++ src/componentes/principal/UltimosLancamentos/index.js
@@ -1,8 +1,44 @@
 import styled from "styled-components";
+import { livros } from "./dadosUltimosLancamentos";
+
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const Titulo = styled.h2`
+    width: 100%;
+    padding: 30px 0;
+    background-color: #FFF;
+    color: #EB9B00;
+    font-size: 36px;
+    text-align: center;
+    margin: 0;
+`
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
 
 function UltimosLancamentos(){
     return(
-        <p2>UltimosLancamentos</p2>
+
+        <UltimosLancamentosContainer>
+            
+            <Titulo>UltimosLancamentos</Titulo>
+                <NovosLivrosContainer>
+                    {livros.map ( livro => (
+                        <img src={livro.src} alt={livro.nome}></img>
+            ))}
+            </NovosLivrosContainer>
+        </UltimosLancamentosContainer>
+        
     )
     
 }
